6f0c25939a93967aa429e56360b831f4706a4102
Leonan <79343444+leonanthomaz@users.noreply.github.com>
2025-02-26 19:23:51 -0300

    Update PrivateRoute.tsx

--- src/routes/PrivateRoute.tsx
+++ src/routes/PrivateRoute.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
-// import { Navigate, Outlet } from 'react-router-dom';
-import { Outlet } from 'react-router-dom';
-// import { useAuth } from '../contexts/AuthContext';
+import { Navigate, Outlet } from 'react-router-dom';
+import { useAuth } from '../contexts/AuthContext';
 
 const PrivateRoute: React.FC = () => {
-  // const { state } = useAuth();
-  // return state.isAuthenticated ? <Outlet /> : <Navigate to="/" replace />;
-  return <Outlet />
+  const { state } = useAuth();
+  return state.isAuthenticated ? <Outlet /> : <Navigate to="/" replace />;
 };
 
 export default PrivateRoute;
